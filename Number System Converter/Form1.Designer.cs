efe6164bfaa1faf2219efa1fefefee669b17bb47
Loganathan Bhavaneetharan <68679905+Bhavan24@users.noreply.github.com>
2021-01-01 19:45:46 +0530

    Add files via upload

---
+++ Number System Converter/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Number_System_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt8 = new System.Windows.Forms.TextBox();
+            this.txt10 = new System.Windows.Forms.TextBox();
+            this.txt16 = new System.Windows.Forms.TextBox();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn16 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(206, 40);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(141, 20);
+            this.txt2.TabIndex = 0;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(353, 38);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Convert";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tw Cen MT Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Binary number (2)       :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tw Cen MT Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Octal number (8)         :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tw Cen MT Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Decimal number (10)  :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tw Cen MT Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(29, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 22);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Hex number (16)         :";
+            // 
+            // txt8
+            // 
+            this.txt8.Location = new System.Drawing.Point(206, 76);
+            this.txt8.Name = "txt8";
+            this.txt8.Size = new System.Drawing.Size(141, 20);
+            this.txt8.TabIndex = 0;
+            // 
+            // txt10
+            // 
+            this.txt10.Location = new System.Drawing.Point(206, 111);
+            this.txt10.Name = "txt10";
+            this.txt10.Size = new System.Drawing.Size(141, 20);
+            this.txt10.TabIndex = 0;
+            // 
+            // txt16
+            // 
+            this.txt16.Location = new System.Drawing.Point(206, 145);
+            this.txt16.Name = "txt16";
+            this.txt16.Size = new System.Drawing.Size(141, 20);
+            this.txt16.TabIndex = 0;
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(353, 74);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 23);
+            this.btn8.TabIndex = 1;
+            this.btn8.Text = "Convert";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn10
+            // 
+            this.btn10.Location = new System.Drawing.Point(353, 109);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(75, 23);
+            this.btn10.TabIndex = 1;
+            this.btn10.Text = "Convert";
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.btn10_Click);
+            // 
+            // btn16
+            // 
+            this.btn16.Location = new System.Drawing.Point(353, 143);
+            this.btn16.Name = "btn16";
+            this.btn16.Size = new System.Drawing.Size(75, 23);
+            this.btn16.TabIndex = 1;
+            this.btn16.Text = "Convert";
+            this.btn16.UseVisualStyleBackColor = true;
+            this.btn16.Click += new System.EventHandler(this.btn16_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(206, 190);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(452, 229);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btn16);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.txt16);
+            this.Controls.Add(this.txt10);
+            this.Controls.Add(this.txt8);
+            this.Controls.Add(this.txt2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Number System Converter (By Bhavan)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt8;
+        private System.Windows.Forms.TextBox txt10;
+        private System.Windows.Forms.TextBox txt16;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn16;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Number System Converter/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Number_System_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            
+            int binaryNumber = int.Parse(txt2.Text);
+            int decimalValue = Convert.ToInt32(binaryNumber.ToString(), 2);
+            txt10.Text = Convert.ToString(decimalValue);
+            int number = int.Parse(txt10.Text);
+            string Base = Convert.ToString(number, 2);
+            txt2.Text = Base;
+            string Base1 = Convert.ToString(number, 8);
+            txt8.Text = Base1;
+            string Base2 = Convert.ToString(number, 16);
+            txt16.Text = Base2.ToUpper();
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            int octnum = 0, r = 0;
+            int i = 0;
+            double decnum = 0;
+            octnum = int.Parse(txt8.Text);
+            while (octnum != 0)
+            {
+                r = octnum % 10;
+                decnum = decnum + (r * Math.Pow(8, i++));
+                octnum = octnum / 10;
+            }
+            txt10.Text = decnum.ToString();
+            int number = int.Parse(txt10.Text);
+            string Base = Convert.ToString(number, 2);
+            txt2.Text = Base;
+            string Base2 = Convert.ToString(number, 16);
+            txt16.Text = Base2.ToUpper();
+        }
+
+        private void btn10_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(txt10.Text);
+            string Base = Convert.ToString(number, 2);
+            txt2.Text = Base;
+            string Base1 = Convert.ToString(number, 8);
+            txt8.Text = Base1;
+            string Base2 = Convert.ToString(number, 16);
+            txt16.Text = Base2.ToUpper();            
+        }
+
+        private void btn16_Click(object sender, EventArgs e)
+        {
+            string hex_value = txt16.Text;
+            int int_value = Convert.ToInt32(hex_value, 16);
+            txt10.Text = Convert.ToString(int_value);
+            int number = int.Parse(txt10.Text);
+            string Base = Convert.ToString(number, 2);
+            txt2.Text = Base;
+            string Base2 = Convert.ToString(number, 8);
+            txt8.Text = Base2;            
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txt2.Clear();
+            txt8.Clear();
+            txt10.Clear();
+            txt16.Clear();
+            txt2.Focus();
+        }
+    }
+}
